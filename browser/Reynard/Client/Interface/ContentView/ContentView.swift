40f9dcb1949d600a366983b736e6dee6d350831f
Minh Ton <37860569+minh-ton@users.noreply.github.com>
2026-06-16 10:18:47 +0700

    Split into extension file

--- browser/Reynard/Client/Interface/ContentView/ContentView.swift
+++ browser/Reynard/Client/Interface/ContentView/ContentView.swift
@@ -343,24 +343,3 @@ final class ContentView: UIView {
     }
 }
 
-private extension UIView {
-    func hasCommonAncestor(with view: UIView) -> Bool {
-        var ancestors = Set<ObjectIdentifier>()
-        var currentView: UIView? = self
-
-        while let view = currentView {
-            ancestors.insert(ObjectIdentifier(view))
-            currentView = view.superview
-        }
-
-        currentView = view
-        while let view = currentView {
-            if ancestors.contains(ObjectIdentifier(view)) {
-                return true
-            }
-            currentView = view.superview
-        }
-
-        return false
-    }
-}
