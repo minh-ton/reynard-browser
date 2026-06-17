a08ba2fbd88a14eaa0a5031e7b676d8b3943a23c
Minh Ton <37860569+minh-ton@users.noreply.github.com>
2026-06-17 23:23:52 +0700

    Even more file split

--- browser/Reynard/Client/Interface/BrowserViewController.swift
+++ browser/Reynard/Client/Interface/BrowserViewController.swift
@@ -8,31 +8,6 @@
 import GeckoView
 import UIKit
 
-struct BrowserLayout: Equatable {
-    enum ViewportOrientation: Equatable {
-        case portrait
-        case landscape
-    }
-
-    let interfaceIdiom: UIUserInterfaceIdiom
-    let orientation: ViewportOrientation
-    let chromeMode: browserChromeMode
-    let chromePosition: browserChromePosition
-    let tabOverviewToolbarPosition: TabOverview.ToolbarPosition
-    let overlayHost: OverlayCoordinator.Host
-
-    static func initial(interfaceIdiom: UIUserInterfaceIdiom) -> BrowserLayout {
-        BrowserLayout(
-            interfaceIdiom: interfaceIdiom,
-            orientation: .portrait,
-            chromeMode: interfaceIdiom == .pad ? .pad : .phone,
-            chromePosition: .bottom,
-            tabOverviewToolbarPosition: interfaceIdiom == .pad ? .top : .bottom,
-            overlayHost: interfaceIdiom == .pad ? .detached : .embedded
-        )
-    }
-}
-
 final class BrowserViewController: UIViewController {
     // MARK: - UX
 
