e57b8453fd8bdaf54f89b5fa80a9dbc979310d67
Minh Ton <37860569+minh-ton@users.noreply.github.com>
2026-06-18 16:26:16 +0700

    Slight rename

--- browser/Reynard/Client/Shared/Haptics.swift
+++ browser/Reynard/Client/Shared/Haptics.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 enum Haptics {
-    private static let rigidGenerator = UIImpactFeedbackGenerator(style: .rigid)
+    private static let impactGenerator = UIImpactFeedbackGenerator(style: .rigid)
     private static let notificationGenerator = UINotificationFeedbackGenerator()
 
     static func prepareRigid() {
-        rigidGenerator.prepare()
+        impactGenerator.prepare()
     }
 
     static func rigid() {
-        rigidGenerator.impactOccurred()
+        impactGenerator.impactOccurred()
     }
 
     static func success() {
